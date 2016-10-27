e58796e4478cf78d8837d8693d02a9784300d5fa
Franchette Camoro <nicolecamoro@gmail.com>
2016-10-28 06:51:38 +0800

    Fixed the navbar

--- js/directives/dir-Nav.js
+++ js/directives/dir-Nav.js
@@ -6,8 +6,19 @@ angular.module('lmsApp')
   var linker = function(scope) {
     var id = firebase.auth().currentUser.uid;
 
+    userTypeData = LoggedInUser.getUsertype();
+
+    if (userTypeData == "student") {
+      scope.utypePath = "#/student_page";
+    }else if (userTypeData == "teacher") {
+      scope.utypePath = "#/teacher/home";
+    }
+
+    console.log(scope.utypePath);
+
     scope.logOutUser = function() {
       firebase.auth().signOut();
+      console.log("signing out");
       $location.path('/');
 
       firebase.auth().onAuthStateChanged(function(user) {
@@ -36,15 +47,15 @@ angular.module('lmsApp')
     // scope.utype_dash;
     // scope.utypePath;
     (function typeToPath() {
-      userTypeData = LoggedInUser.getUsertype();
-
-      if (userTypeData == "student") {
-        scope.utypePath = "#/student_page";
-      }else if (userTypeData == "teacher") {
-        scope.utypePath = "#/teacher/home";
-      }
-
-      console.log(scope.utypePath);
+      // userTypeData = LoggedInUser.getUsertype();
+      //
+      // if (userTypeData == "student") {
+      //   scope.utypePath = "#/student_page";
+      // }else if (userTypeData == "teacher") {
+      //   scope.utypePath = "#/teacher/home";
+      // }
+      //
+      // console.log(scope.utypePath);
     })()
     //
     // function setUtype_Path(path) {
@@ -57,12 +68,12 @@ angular.module('lmsApp')
     }
 
     scope.$watch('utypePath', function() {
-      
+
     });
 
     scope.path = '#/profile/' + id;
     scope.name = firebase.auth().currentUser.displayName;
-    scope.id = id;
+    scope.userid = id;
 
     // scope.NotifNum = 3;
 
